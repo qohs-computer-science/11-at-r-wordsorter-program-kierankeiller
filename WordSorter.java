02d0076728fe333c3a7ad063edf6d9d20d82a857
kierankeiller <kieran.keiller@gmail.com>
2024-10-07 14:21:28 +0000

    submission 10/7

--- WordSorter.java
+++ WordSorter.java
@@ -9,19 +9,19 @@ public class WordSorter
 		try
 		{
 			in = new Scanner(new File("article.txt"));	
-		}
+		}//end try
 		catch(Exception e)
 		{
 			System.out.println("Cannot find file... Exiting Program");
 			return;
-		}	
+		}//end catch
 		String word = "";
 
 
 		ArrayList [] alphabet = new ArrayList [26];
 		for(int i = 0; i<26; i++){
 			alphabet[i] = new ArrayList <String> ();
-		}
+		}//end for loop
 		int q = 0;
 
 		while(in.hasNext())
@@ -80,44 +80,67 @@ public class WordSorter
 				for (int i = 0; i<alphabet[index].size(); i++){
 					System.out.println(alphabet[index].get(i));
 				}//end for loop
-			}
+				if (alphabet[index].size() == 0){
+					System.out.println("No words start with this letter");
+				}//end if
+			}//end answer 1 if
+
 			else if (answer == 2){
 				for(int i = 0; i<alphabet.length; i++){
 					for(int j = 0; j<alphabet[i].size(); j++){
 						System.out.println(alphabet[i].get(j));
 					}//end inner for
 				}//end outer for
-			}
+				if (q==0){
+					System.out.println("Empty List");
+				}//end if
+			}//end answer 2 if
+
 			else if (answer == 3){
 				System.out.println("There are " + q + " unique words in the article");
-			}
+			}//end answer 3 if
+
 			else if (answer == 4){
 				System.out.println("What word would you like to search for?");
 				String inWord = input.nextLine();
 				int index = 0;
 				index = inWord.charAt(0)-97;
+				boolean found = false;
 				for (int i = 0; i<alphabet[index].size(); i++){
-					if (alphabet[index].get(i).equals(inWord));{
-						System.out.println("Word found in the article");
-					}
-					else
-
-
-
-
-
-
-
-
-
-
-
-					
+					if (alphabet[index].get(i).equals(inWord)){
+						found = true;
+					}//end if
 				}//end for loop
-			}
+				if(found == false){
+					System.out.println("Word NOT found in the article.");
+				}//end if
+				else {
+					System.out.println("Word found in the article");
+				}// end else
+			}//end answer 4 if
+
 			else if (answer == 5){
-				
-			}
+				System.out.println("What word would you like to remove?");
+				String inWord = input.nextLine();
+				int index = 0;
+				index = inWord.charAt(0)-97;
+				boolean found = false;
+				for (int i = 0; i<alphabet[index].size(); i++){
+					if (alphabet[index].get(i).equals(inWord)){
+						found = true;
+						alphabet[index].remove(i);
+						q--;
+					}//end if
+				}//end for loop
+				if(found == false){
+					System.out.println("Word NOT found in the article.");
+				}//end if
+				else {
+					System.out.println("Word successfully removed from the list");
+				}//end else
+			}//end answer 5 if
+
+			System.out.println();
 		}//end while
 
 	}//end main
